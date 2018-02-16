700861c8cdcb2e8ff6644e5c90ccc2eeaead1699
rachapipe <pipe_channarong@hotmail.com>
2018-02-16 18:20:39 +0700

    เพิ่มคำสั่ง แก้ไขเวลาเริ่มโปรแกรม
    -เพิ่มคำสั่งDate
    -ใช้คำสั่งquit แล้วมีการclearหน้าจอให้
    -เวลาเข้าโปรแกรม ให้เด้งไปที่หน้าHomeของuserตัวเอง

--- simpleshell.c
+++ simpleshell.c
@@ -147,7 +147,50 @@ void launchProg(char **args, int background){
 		 printf("Process created with PID: %d\n",pid);
 	 }	 
 }
-
+ 
+/**
+* Method used to manage I/O redirection
+*/ 
+void fileIO(char * args[], char* inputFile, char* outputFile, int option){
+	 
+	int err = -1;
+	
+	int fileDescriptor; // between 0 and 19, describing the output or input file
+	
+	if((pid=fork())==-1){
+		printf("Child process could not be created\n");
+		return;
+	}
+	if(pid==0){
+		// Option 0: output redirection
+		if (option == 0){
+			// We open (create) the file truncating it at 0, for write only
+			fileDescriptor = open(outputFile, O_CREAT | O_TRUNC | O_WRONLY, 0600); 
+			// We replace de standard output with the appropriate file
+			dup2(fileDescriptor, STDOUT_FILENO); 
+			close(fileDescriptor);
+		// Option 1: input and output redirection
+		}else if (option == 1){
+			// We open file for read only (it's STDIN)
+			fileDescriptor = open(inputFile, O_RDONLY, 0600);  
+			// We replace de standard input with the appropriate file
+			dup2(fileDescriptor, STDIN_FILENO);
+			close(fileDescriptor);
+			// Same as before for the output file
+			fileDescriptor = open(outputFile, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+			dup2(fileDescriptor, STDOUT_FILENO);
+			close(fileDescriptor);		 
+		}
+		 
+		setenv("parent",getcwd(currentDirectory, 1024),1);
+		
+		if (execvp(args[0],args)==err){
+			printf("err");
+			kill(getpid(),SIGTERM);
+		}		 
+	}
+	waitpid(pid,NULL,0);
+}
 
 /**
 * Method used to manage pipes.
@@ -325,9 +368,14 @@ int commandHandler(char * args[]){
 		args_aux[j] = args[j];
 		j++;
 	}
-	
+	// 'date' command date	
+	if(strcmp(args[0],"date")== 0) system("date +%A%t%D%t%H:%M:%S");
 	// 'quit' command quits the shell
-	if(strcmp(args[0],"quit") == 0) exit(0);
+	else if(strcmp(args[0],"quit") == 0) 
+	{
+		system("clear");
+		exit(0);
+	}
  	// 'clear' command clears the screen
 	else if (strcmp(args[0],"clear") == 0) system("clear");
 	// 'cd' command to change directory
@@ -438,10 +486,10 @@ int main(int argc, char *argv[], char ** envp) {
 	no_reprint_prmpt = 0; 	// to prevent the printing of the shell
 							// after certain methods
 	pid = -10; // we initialize pid to an pid that is not possible
-	
+	chdir(getenv("HOME")); //
 	// We call the method of initialization, clearing the screen and the welcome screen
 	init();
-    clearScreen();
+    	clearScreen();
 	welcomeScreen(argc);
 	
     
