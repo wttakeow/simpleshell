e5312648af3dc10bcd88139d4bc971582eaa78bd
Wisoodthipong Takeow <wttakeow@gmail.com>
2018-02-16 18:27:03 +0700

    cleaning up

--- simpleshell.c
+++ simpleshell.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -147,50 +146,7 @@ void launchProg(char **args, int background){
 		 printf("Process created with PID: %d\n",pid);
 	 }	 
 }
- 
-/**
-* Method used to manage I/O redirection
-*/ 
-void fileIO(char * args[], char* inputFile, char* outputFile, int option){
-	 
-	int err = -1;
-	
-	int fileDescriptor; // between 0 and 19, describing the output or input file
-	
-	if((pid=fork())==-1){
-		printf("Child process could not be created\n");
-		return;
-	}
-	if(pid==0){
-		// Option 0: output redirection
-		if (option == 0){
-			// We open (create) the file truncating it at 0, for write only
-			fileDescriptor = open(outputFile, O_CREAT | O_TRUNC | O_WRONLY, 0600); 
-			// We replace de standard output with the appropriate file
-			dup2(fileDescriptor, STDOUT_FILENO); 
-			close(fileDescriptor);
-		// Option 1: input and output redirection
-		}else if (option == 1){
-			// We open file for read only (it's STDIN)
-			fileDescriptor = open(inputFile, O_RDONLY, 0600);  
-			// We replace de standard input with the appropriate file
-			dup2(fileDescriptor, STDIN_FILENO);
-			close(fileDescriptor);
-			// Same as before for the output file
-			fileDescriptor = open(outputFile, O_CREAT | O_TRUNC | O_WRONLY, 0600);
-			dup2(fileDescriptor, STDOUT_FILENO);
-			close(fileDescriptor);		 
-		}
-		 
-		setenv("parent",getcwd(currentDirectory, 1024),1);
-		
-		if (execvp(args[0],args)==err){
-			printf("err");
-			kill(getpid(),SIGTERM);
-		}		 
-	}
-	waitpid(pid,NULL,0);
-}
+
 
 /**
 * Method used to manage pipes.
@@ -351,10 +307,7 @@ void shellPrompt(){
 int commandHandler(char * args[]){
 	int i = 0;
 	int j = 0;
-	
-	int fileDescriptor;
-	int standardOut;
-	
+		
 	int aux;
 	int background = 0;
 	
